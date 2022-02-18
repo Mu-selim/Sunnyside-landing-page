c576f25b7d7b08bfa55a73964a3359a124c55e3a
Muhammad Selim <muhamadselim70@gmail.com>
2022-02-18 15:14:00 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", function() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.
+addEventListener("click", function() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
+
 var mess = "we are creatives"
 i = 0
 
