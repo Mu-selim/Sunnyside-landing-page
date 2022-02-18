889a2b7401a7e563e563f24cfacfb32bec0baaad
Muhammad Selim <muhamadselim70@gmail.com>
2022-02-18 11:57:32 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
-var typing = document.getElementById("typing")
 var mess = "we are creatives"
-var color = document.querySelector('nav-links')
 i = 0
 
 timer = setInterval(function () {
     if (i >= mess.length)
     {
         i = 0
-        typing.textContent = ''
+        document.getElementById("typing").textContent = ""
     }
-    typing.textContent += mess[i] 
-    i += 1
+    document.getElementById("typing").textContent += mess[i];
+    i = i + 1;
 }, 300)
 
